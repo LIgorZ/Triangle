dff3c3e3fc72967f034541e0f4e4137db3e60df1
LIgorZ <96433539+LIgorZ@users.noreply.github.com>
2021-12-26 14:21:48 +0300

    Ver 1.1Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Triangle;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NET.C#.04 Создание новых типов в C#");
+            int a, b, c;
+
+            Console.Write("Введите длину строны а: ");
+            a = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите длину строны b: ");
+            b = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите длину строны c: ");
+            c = Int32.Parse(Console.ReadLine());
+
+            Triangle obj = new Triangle(a, b, c);
+            Console.WriteLine("Вывести длины сторон треугольника на экран:");
+            obj.Show();
+            Console.WriteLine();
+
+            Console.Write("Периметр треугольника: ");
+            Console.WriteLine(obj.Perimeter());
+            Console.WriteLine();
+
+            Console.Write("Площадь треугольника: ");
+            Console.WriteLine(obj.Area());
+            Console.WriteLine();
+
+            Console.WriteLine("Получить-установить длины сторон треугольника:");
+            obj.A = 7;
+            obj.B = 7;
+            obj.C = 7;
+            obj.Show();
+            Console.WriteLine();
+
+            Console.WriteLine("Позволяющее установить," +
+                "существует ли треугольник с данными длинами сторон:");
+            Console.WriteLine(obj.isTriangle);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Triangle
+{
+    class Triangle
+    {
+        
+        // Поля: int a, b, c;
+        int a, b, c;
+
+        //Конструктор, позволяющий создать экземпляр класса  с заданными длинами сторон.
+        public Triangle(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public Triangle() { }
+
+        // Методы, позволяющие: вывести длины сторон треугольника на экран;
+        public void Show() { Console.WriteLine("Треугольник со сторонами а = {0}, b = {1}, c = {2}", a, b, c); }
+
+        //расчитать периметр треугольника;
+        public int Perimeter() { return a + b + c; }
+
+        // расчитать площадь треугольника
+        public double Area()
+        {
+            double HalfPerim = (double)(a + b + c) / 2;
+            return Math.Sqrt(HalfPerim * (HalfPerim - a) * (HalfPerim - b) * (HalfPerim - c));
+        }
+
+        // Свойства: позволяющее получить-установить длины сторон треугольника (доступное для чтения и записи);
+        public int A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+
+        public int B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+
+        public int C
+        {
+            get { return c; }
+            set { c = value; }
+        }
+
+        // позволяющее установить, существует ли треугольник с данными длинами сторон (доступное только для чтения).
+        public bool isTriangle
+        {
+            get
+            {
+                if (a + b > c && a + c > b && b + c > a)
+                    return true;
+                return false;
+            }
+        }
+
+        // Индексатор, позволяющий по индексу 0 обращаться к полю a, по индексу 1 – к полю b, по индексу 2 – к полю c, при других значениях индекса выдается сообщение об ошибке.
+        public int this[int index]
+        {
+            get
+            {
+                switch (index)
+                {
+                    case 1:
+                        return a;
+                    case 2:
+                        return b;
+                    case 3:
+                        return c;
+                    default:
+                        throw new Exception("Error");
+                }
+            }
+        }
+
+    }
+}
